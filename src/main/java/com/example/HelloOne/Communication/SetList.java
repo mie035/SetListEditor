dcb0ba220eabba01002b250aea58a777219db987
mie035 <Notakes035@gmail.com>
2022-04-14 17:14:08 +0900

    design data class for setlist editting

---
+++ src/main/java/com/example/HelloOne/Communication/SetList.java
@@ -0,0 +1,38 @@
+package com.example.HelloOne.Communication;
+
+import java.util.List;
+import java.util.Collections;
+
+public class SetList {
+    private int Id;
+    private String Name;
+    private List<String> FilePathList;
+
+    public SetList(String name, int id, List<String> filePathList) {
+        Name = name;
+        Id = id;
+        FilePathList = filePathList;
+    }
+    public int getId() {
+        return Id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    /**
+     * return file refered list.this is unmodifiableList object.
+     * @return
+     */
+    public List<String> getFIlePathList()
+    {
+        final List<String> lst = Collections.unmodifiableList(FilePathList);
+        return lst;
+    }
+    public void setFilePathList(List<String> filePathList)
+    {
+        FilePathList = filePathList;
+    }
+}
---
+++ src/main/java/com/example/HelloOne/Communication/Tune.java
@@ -0,0 +1,55 @@
+package com.example.HelloOne.Communication;
+/**
+ * song data class
+ */
+public class Tune {
+    private int Id;
+    private String Name;
+    private int Time;
+    private String Ref;
+
+    public Tune() {
+    }
+
+    public Tune(int id, String name, int time, String ref) {
+        this.Id = id;
+        this.Name = name;
+        this.Time  = time;
+        this.Ref = ref;
+    }
+
+    public int getId() {
+        return Id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public int getTime() {
+        return Time;
+    }
+
+    /**
+     * Update Time for the tune
+     * @param time is expressed by seconds
+     */
+    public void setTime(int time) {
+         Time = time;
+    }
+    /**
+     * Get audio file path for the tune
+     * @return is audio file path
+     */
+    public String getRef() {
+        return Ref;
+    }
+    /**
+     * Update audio file for the tune
+     * @param ref is audio file path
+     */
+    public void setRef(String ref) {
+        Ref = ref;
+    }
+}
